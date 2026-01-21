03c3328a0e2f1a9cefbff26b72622d44eebe4018
GuilhermeSallesBlanco <guilherme.salles.blanco@gmail.com>
2026-01-21 20:00:27 -0300

    LED piscando, alterações em UART e pastas

--- ProgramasC/testeUART/testeUART.c
+++ ProgramasC/testeUART/testeUART.c
@@ -1,24 +1,15 @@
-
-#include <uart.h>
 #include <csr.h>
+#include <uart.h>
+#define printf uart_printf
 #include <mul.h>
 
-static void send_pattern(void) {
-    // 0x55 ('U') -> padrão 10101010, ótimo para medir o bit-time
-    for (int i = 0; i < 64; i++) uart_putc('U');
-    uart_puts("\r\nHello TinyQV!\r\n");
-}
+int a = 3;
 
-int main(void) {
-    send_pattern();
-
-    int a = 3;
-    uart_printf("Hello %d\r\n", a);
-    a = mul32x16(12, a);
-    uart_printf("Hello %d\r\n", a);
-    uart_printf("Cycles %u, time %u, instret %u\r\n",
-                read_cycle(), read_time(), read_instret());
-
-    while (1) { } // mantém a app viva
-    return 0;
+int main() {
+  printf("Hello, world!\n");
+  printf("Hello %d\n", a);
+  a = mul32x16(12, a);
+  printf("Hello %d\n", a);
+  printf("Cycles %d, time %d, instret %d\n", read_cycle(), read_time(), read_instret());
+  return 0;
 }
